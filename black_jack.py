a8bbbf06ad82688fe0da5305812115dc3060cf2d
hiruyhadgu <65324470+hiruyhadgu@users.noreply.github.com>
2022-03-17 21:27:27 -0400

    single player

--- black_jack.py
+++ black_jack.py
@@ -1,6 +1,7 @@
 from audioop import add
 from copy import copy
 import random
+import matplotlib.pyplot as plt
 # GLOBAL VARIABLES values, suits, and ranks ARE USED THROUGHOUT THE CODE. THE PART STILL UNCLEAR
 # IS HOW TO ASSIGN A ONE OR 11 TO ACE
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight': 8, 
@@ -24,7 +25,7 @@ class Players:
 
     while True:
         try:
-            number_of_players = int(input("How many players? "))
+            number_of_players = 1
             break
         except ValueError:
             print("Please input integer only...")
@@ -51,13 +52,9 @@ class Players:
         return self.new_card
     
     def deal_hand(self):
-        
-        no_cards = 2
         deal_cards=[]
-        for i in range(no_cards):
-            dealt_card = Players.new_hand(new_deck)
-            deal_cards.append((dealt_card.suit, dealt_card.rank,dealt_card.value))
-            i+=1
+        dealt_card = Players.new_hand(new_deck)
+        deal_cards.append((dealt_card.suit, dealt_card.rank,dealt_card.value))
         return deal_cards
 
     def betting(self):
@@ -77,7 +74,7 @@ class Decks:
     #specify number of decks to play with
     while True:
         try:
-            number_of_decks = int(input("Dealer inputs number of decks: "))
+            number_of_decks = 1
             break
         except ValueError:
             print("Please input integer only...")
@@ -129,135 +126,133 @@ new_deck = Decks()
 new_deck.shuffle()
 
 # Input player name and buy in amount
-players={}
-for _ in range(Players.number_of_players):
-    player_info = Players.input_player_info()
-    players[player_info.player_id] = int(player_info.buyin_amount)
+players={'one':100}
+#for _ in range(Players.number_of_players):
+#    player_info = Players.input_player_info()
+#    players[player_info.player_id] = int(player_info.buyin_amount)
 
 # Players place minimum bets
 minimum_bet = [5, 10, 15, 20]
 game_over = False
-while game_over == False:
-    make_bet = {}
-    for key in players:
-        make_bet[key] = minimum_bet
-        players[key]= players[key] - make_bet[key]
-    print(players)
+for min_bet in minimum_bet:
+    rounds=0
+    no_rounds = []
+    dollars_per_round = []
+    while game_over == False:
+        make_bet = {}
+        for key in players:
+            make_bet[key] = min_bet
+            players[key]= players[key] - make_bet[key]
+        print(players)
 
-    # Deal the first card for dealer
-    dealer_hand={}
-    dealer_card_no=1
-    card_no = 'card_no_' + str(dealer_card_no)
-    dealer_hand[card_no]=DealerHand.dealer_cards(dealer_hand)
-    dealer_first_card = dealer_hand[card_no]
-    sum_of_cards = 0
-    there_was_ace = False
-    if dealer_first_card.rank == 'Ace':
-            ace_tuple = dealer_first_card.value
-            sum_of_cards=int(ace_tuple[1])
-            there_was_ace = True
-    else:
-            sum_of_cards = int(dealer_first_card.value)
-
-    #card_no = 'card_no_' + '2'
-    #dealer_hand[card_no]=DealerHand.dealer_cards(dealer_hand)
-    #dealer_next_card = dealer_hand[card_no]
-
-    # Deal first two hands for players
-    dealt_hand={}
-    for key in players:
-        dealt_hand[key] = Players.deal_hand(key)
-
-    # Player decision on whether to hit stay or fold
-    player_card_sum=0
-    add_cards = {}
-    for key, value in dealt_hand.items():
-        set_of_hands = dealt_hand[key]
-        for n in range(len(set_of_hands)):
-                if set_of_hands[n][1] == 'Ace':
-                    print(dealt_hand[key])
-                    ace_1_or_11 = input(f'Player {key}, Card No {n+1} is an Ace. Count Ace as 1 or 11?: ')
-                    player_card_sum = player_card_sum + int(ace_1_or_11)
-                    set_of_hands[n] = list(set_of_hands[n])
-                    set_of_hands[n][2]=ace_1_or_11
-                    set_of_hands[n] = tuple(set_of_hands[n])
-                else:
-                    player_card_sum = player_card_sum + int(set_of_hands[n][2])
-        add_cards[key]=player_card_sum
-        player_card_sum=0
-        print(add_cards)
-
-    for key, value in dealt_hand.items():
-        player_card_sum=0
-        print(dealt_hand)
-        hit_stay_or_fold = Players.h_s_f(key)
-        while hit_stay_or_fold == 1:
-            dealt_hand[key]=DealerHand.play_next_hand(dealt_hand[key])
-            set_of_hands = dealt_hand[key]
-            for n in range(2,len(set_of_hands)):
-                if set_of_hands[n][1] == 'Ace':
-                    print(dealt_hand[key])
-                    ace_1_or_11 = input(f'Player {key}, Card No {n+1} is an Ace. Count Ace as 1 or 11?: ')
-                    player_card_sum = player_card_sum + int(ace_1_or_11)
-                    set_of_hands[n] = list(set_of_hands[n])
-                    set_of_hands[n][2]=ace_1_or_11
-                    set_of_hands[n] = tuple(set_of_hands[n])
-                else:
-                    player_card_sum = player_card_sum + int(set_of_hands[n][2])
-            print(set_of_hands)
-            hit_stay_or_fold = Players.h_s_f(key)     
-        add_cards[key] = add_cards[key]+player_card_sum
-        
-
-    #    player_card_sum=0
-    print(add_cards)
-    dealer_sum ={}
-  # Dealer turns dealer's next cards
-    while sum_of_cards <= 16:
-        dealer_card_no+=1
+        # Deal the first card for dealer
+        dealer_hand={}
+        dealer_card_no=1
         card_no = 'card_no_' + str(dealer_card_no)
         dealer_hand[card_no]=DealerHand.dealer_cards(dealer_hand)
-        dealer_next_card = dealer_hand[card_no]
-        if dealer_next_card.rank == 'Ace' and there_was_ace == True:
-            ace_tuple = dealer_next_card.value
-            sum_of_cards=sum_of_cards + int(ace_tuple[0])
-        elif dealer_next_card.rank == 'Ace' and there_was_ace == False:
-            if sum_of_cards <=10:
-                ace_tuple = dealer_next_card.value
-                sum_of_cards=sum_of_cards + int(ace_tuple[1])
-            elif sum_of_cards > 10:
+        dealer_first_card = dealer_hand[card_no]
+        sum_of_cards = 0
+        there_was_ace = False
+        if dealer_first_card.rank == 'Ace':
+                ace_tuple = dealer_first_card.value
+                sum_of_cards=int(ace_tuple[1])
+                there_was_ace = True
+        else:
+                sum_of_cards = int(dealer_first_card.value)
+
+        #card_no = 'card_no_' + '2'
+        #dealer_hand[card_no]=DealerHand.dealer_cards(dealer_hand)
+        #dealer_next_card = dealer_hand[card_no]
+
+        # Deal first two hands for players
+        dealt_hand={}
+        set_of_cards=[]
+        player_card_no=1
+        for key in players:
+            card_no = 'card_no_' + str(player_card_no)
+            player_card_sum=0
+            add_cards = {}
+            hit_stay_or_fold=1
+            if player_card_sum >= 21:
+                hit_stay_or_fold = 2
+            while hit_stay_or_fold == 1:
+                dealt_hand[key] = Players.deal_hand(key)
+                set_of_hands=dealt_hand[key]
+                temp_va = set_of_hands[0][1]
+                if set_of_hands[0][1] == 'Ace':
+                    if player_card_sum <= 10:
+                        ace_1_or_11 = 11
+                    else:
+                        ace_1_or_11 = 1
+                    player_card_sum = player_card_sum + int(ace_1_or_11)
+                    temp_list = list(set_of_hands[0])
+                    temp_list[2]= ace_1_or_11
+                    set_of_hands[0]=temp_list
+                else:
+                    player_card_sum = player_card_sum + int(set_of_hands[0][2])
+                add_cards[key]=player_card_sum
+                set_of_cards.append(tuple(set_of_hands[0]))
+                if player_card_sum >= 21:
+                    hit_stay_or_fold = 2     
+                add_cards[key] = add_cards[key]+player_card_sum
+            print(add_cards)
+            
+
+        #    player_card_sum=0
+        print(add_cards)
+        dealer_sum ={}
+    # Dealer turns dealer's next cards
+        while sum_of_cards <= 16:
+            dealer_card_no+=1
+            card_no = 'card_no_' + str(dealer_card_no)
+            dealer_hand[card_no]=DealerHand.dealer_cards(dealer_hand)
+            dealer_next_card = dealer_hand[card_no]
+            if dealer_next_card.rank == 'Ace' and there_was_ace == True:
                 ace_tuple = dealer_next_card.value
                 sum_of_cards=sum_of_cards + int(ace_tuple[0])
-        else:
-            sum_of_cards = sum_of_cards + int(dealer_next_card.value)
-        dealer_sum['dealer_hand']=sum_of_cards
-    
-    for key, value in dealer_hand.items():
-        print(key, value)
+            elif dealer_next_card.rank == 'Ace' and there_was_ace == False:
+                if sum_of_cards <=10:
+                    ace_tuple = dealer_next_card.value
+                    sum_of_cards=sum_of_cards + int(ace_tuple[1])
+                elif sum_of_cards > 10:
+                    ace_tuple = dealer_next_card.value
+                    sum_of_cards=sum_of_cards + int(ace_tuple[0])
+            else:
+                sum_of_cards = sum_of_cards + int(dealer_next_card.value)
+            dealer_sum['dealer_hand']=sum_of_cards
+        
+        for key, value in dealer_hand.items():
+            print(key, value)
 
-    if sum_of_cards > 21:
-        for key, value in add_cards.items():
-            if value <= 21:
-                players[key]=players[key]+make_bet[key]*2
-    elif sum_of_cards == 21:
-        for key, value in add_cards.items():
-            if value == 21:
-                players[key] = players[key]+make_bet[key]
-    elif sum_of_cards < 21:
-        for key, value in add_cards.items():
-            if value <= 21 and value > sum_of_cards:
-                players[key]=players[key]+make_bet[key]*2
-            elif value <= 21 and value == sum_of_cards:
-                players[key]=players[key]+make_bet[key]
+        if sum_of_cards > 21:
+            for key, value in add_cards.items():
+                if value <= 21:
+                    players[key]=players[key]+make_bet[key]*2
+        elif sum_of_cards == 21:
+            for key, value in add_cards.items():
+                if value == 21:
+                    players[key] = players[key]+make_bet[key]
+        elif sum_of_cards < 21:
+            for key, value in add_cards.items():
+                if value <= 21 and value > sum_of_cards:
+                    players[key]=players[key]+make_bet[key]*2
+                elif value <= 21 and value == sum_of_cards:
+                    players[key]=players[key]+make_bet[key]
 
 
-    print(dealer_sum)
-    print(add_cards)
-    print(players)               
-       
-    for key in players.copy():
-       decide = Players.keep_playing(key)
-       if decide == 'n':
-           players.pop(key)
-    if players=={}:
-        game_over=True
+        print(dealer_sum)
+        print(add_cards)
+        print(players)               
+        
+ #       for key in players.copy():
+  #          decide = 'y'
+ #           if decide == 'n':
+  #              players.pop(key)
+        rounds+=1
+        no_rounds.append(rounds)
+        dollars_per_round.append(players[key])
+        if players[key] < 5:
+            game_over=True
+print(no_rounds)
+print(dollars_per_round)
+plt.plot(no_rounds, dollars_per_round)
