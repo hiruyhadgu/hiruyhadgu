7d69eea428182759d7d40f5e2277d8caeac0939c
hiruyhadgu <65324470+hiruyhadgu@users.noreply.github.com>
2022-06-08 23:02:51 -0400

    added directors to organize files for each district and county executive race to allow viewing of funds by district

--- app.py
+++ app.py
@@ -7,16 +7,28 @@ import openpyxl
 
 
 st.set_page_config(page_title='Howard County Council Candidate Contributions')
-st.header('2022 Primary Elections')
-st.subheader('Know who is contributing in your local elections')
+#st.header('Howard County 2022 Elections for County Executive and County Council')
+st.markdown("<h1 style='text-align: center; color: #007af9;'>Howard County 2022 Elections \
+for County Executive and County Council</h1>", unsafe_allow_html=True)
+st.write('Howard County will hold its primary elections on July 19, 2022.\
+            Council seats in District 1 and District 4 and the County Executive race.\
+                This page show which candidates rely on contributions for developers.')
 
 ### --- LOAD DATAFRAME
-data_file_folder = './'
+
+menu = ['County Executive CE', 'County Council D1', 'County Council D2', 'County Council D3', 'County Council D4', 'County Council D5']
+st.sidebar.header('Choose Election')
+choice = st.sidebar.selectbox('Pick Race:', menu)
+st.subheader(f'Viewing: {choice} Filing')
+
+foldername = choice.split(' ')[-1]
+data_file_folder = f'./{foldername}'
 df = []
 
 for file in os.listdir(data_file_folder):
     if file.endswith('.xlsx'):
         sheet_name, extension = file.split('.')
+        file = f'{data_file_folder}/{file}'
         df.append(pd.read_excel(file, sheet_name = sheet_name, usecols = 'A:J'))
 df_master = pd.concat(df, axis=0)
 
@@ -24,10 +36,6 @@ candidate_name = df_master['Receiving Committee'].unique().tolist()
 donor_name = df_master['Contributor Name'].unique().tolist()
 contributions = df_master['Contribution Amount'].unique().tolist()
 filing_period = df_master['Filing Period'].unique().tolist()
-
-menu = ['County Executive', 'County Council D1', 'County Council D2', 'County Council D3', 'County Council D4', 'County Council D5']
-st.sidebar.header('Choose Election')
-st.sidebar.selectbox('Pick Race:', menu)
 
 selected_period = st.selectbox('Filing Period:', filing_period)
 
