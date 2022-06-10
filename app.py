c347c87d16b5d36e7313e31ef4bd0006d5acfd1c
hiruyhadgu <65324470+hiruyhadgu@users.noreply.github.com>
2022-06-10 17:58:17 -0400

    added AgGrid options

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import os
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from PIL import Image
-import openpyxl
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
-
+### --- SET UP THE PAGE
 st.set_page_config(page_title='Howard County Council Candidate Contributions')
-#st.header('Howard County 2022 Elections for County Executive and County Council')
 st.markdown("<h1 style='text-align: center; color: #007af9;'>Howard County 2022 Elections \
 for County Executive and County Council</h1>", unsafe_allow_html=True)
 st.write('Howard County will hold its primary elections on July 19, 2022.\
@@ -16,7 +14,7 @@ st.write('Howard County will hold its primary elections on July 19, 2022.\
 
 ### --- LOAD DATAFRAME
 
-menu = ['County Council D1','County Executive CE', 'County Council D2', 'County Council D3', 'County Council D4', 'County Council D5']
+menu = ['County Council D1', 'County Council D2', 'County Council D3', 'County Council D4', 'County Council D5', 'County Executive CE']
 st.sidebar.header('Choose Election')
 choice = st.sidebar.selectbox('Pick Race:', menu)
 st.subheader(f'Viewing: {choice} Filing')
@@ -48,6 +46,8 @@ candidate_selection = st.multiselect('Candidate Name:',
                                     candidate_name,
                                     default = candidate_name)
 
+### --- LOAD DEVELOPER LIST TO CROSS REFERENCE
+
 developer_list = pd.read_csv('developercrossreference.csv').drop_duplicates()
 developer_list = developer_list.dropna()
 developer_list['Developer/Developer Affiliated']=developer_list['Developer/Developer Affiliated'].str.lower()
@@ -59,6 +59,9 @@ developer_filter=df_master['Contributor Name'].isin(developer_list['Contributor 
 developer_contributions = df_master[developer_filter]
 developer = st.checkbox('Display Developer Contributions Only')
 
+
+### --- DEFINING THE FILTER CRITERIA. IF 'developer' IS SELECTED, THE FILTER WILL INCLUDE ONLY DEVELOPERS
+
 if developer:
      mask =(df_master['Contributor Name'].isin(developer_list['Contributor Name']))&(df_master['Filing Period']==selected_period) & (df_master['Contribution Amount'].between(*contributions_selection)) & (
                                     df_master['Receiving Committee'].isin(candidate_selection))
@@ -69,15 +72,67 @@ number_of_results = df_master[mask].shape[0]
 sum_of_results = df_master[mask]['Contribution Amount'].sum()
 st.markdown(f'*Number of Contributions: {number_of_results}*')
 st.markdown(f'*Total Contribution for Selected Range: ${sum_of_results:,.2f}*')
-st.dataframe(df_master[mask])
+#st.dataframe(df_master[mask])
+#AgGrid(df_master[mask])
+df_master['Contribution Amount']=df_master['Contribution Amount'].astype(float).apply('${:,.2f}'.format)
 
-df_contribution_sum = df_master[mask].groupby(by = ['Contributor Name', 'Receiving Committee']).sum()[['Contribution Amount']]
-df_contribution_count = df_master[mask].groupby(by = ['Contributor Name','Receiving Committee']).count()[['Contribution Amount']]
+
+gb = GridOptionsBuilder.from_dataframe(df_master[mask])
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    df_master[mask],
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=450, 
+    #width='100%',
+    reload_data=True
+)
+
+data = grid_response['data']
+selected = grid_response['selected_rows'] 
+df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+
+df_filtered = df_master[mask]
+
+df_contribution_sum = df_filtered.groupby(['Contributor Name', 'Receiving Committee']).sum()[['Contribution Amount']]
+df_contribution_count = df_filtered.groupby(['Contributor Name','Receiving Committee']).count()[['Contribution Amount']]
 df_contribution_count = df_contribution_count.rename(columns = {'Contribution Amount':'No of Contributions'})
 df_grouped = [df_contribution_sum, df_contribution_count]
 df_grouped = pd.concat(df_grouped, axis = 1)
 df_grouped = df_grouped.sort_values(by=['No of Contributions'], ascending = False)
-#df_grouped.columns = df_grouped.columns.astype('str')
-# df_grouped = df_grouped.unstack()
+df_grouped.columns = df_grouped.columns.astype('str')
+#df_grouped = df_grouped.unstack()
 #st.sidebar.selectbox("Select Candidate Name:", candidate_name)
-st.dataframe(df_grouped.reset_index())
+# AgGrid(df_grouped.reset_index())
+
+gb = GridOptionsBuilder.from_dataframe(df_grouped.reset_index())
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    df_grouped.reset_index(),
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=450, 
+    #width='100%',
+    reload_data=True
+)
+
+data = grid_response['data']
+selected = grid_response['selected_rows'] 
+df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+st.dataframe(df)
